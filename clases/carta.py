71f704d6c6d3fa3a87360346a5b210e4e1c5f5bf
Josep <hackerjosep4.yt@gmail.com>
2025-10-30 14:23:37 +0100

    Update carta class
    create some functions for carta class

--- clases/carta.py
+++ clases/carta.py
@@ -3,3 +3,40 @@ class carta:
     def __init__(self, p, n):
         self.palo = p
         self.numero = n
+    
+    def strp(self, p):
+        if p == 0:
+            return "♠"
+        elif p == 1:
+            return "♥"
+        elif p == 2:
+            return "♣"
+        elif p == 3:
+            return "♦"
+        else:
+            return "-"
+    def strp(self):
+        return self.strp(self.palo)
+    
+    def strn(self, n):
+        if n == 1:
+            return "A"
+        elif n == 11:
+            return "J"
+        elif n == 12:
+            return "Q"
+        elif n == 13:
+            return "K"
+        elif n > 0 and n < 14:
+            return str(n)
+        else:
+            return "-"
+    def strn(self):
+        return self.strn(self.numero)
+    
+    def __str__(self, pretty):
+        if pretty:
+            text = self.strp()+" "+self.strn()
+        else:
+            text = str(self.palo)+" "+str(self.numero)
+        return text
